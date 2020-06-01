8bb4cc335ddddad4f48cc433cab74a254e53351d
Gilbert <gilbertkitetu1@gmail.com>
2020-06-01 12:21:43 +0300

    First commit

---
+++ config.js
@@ -0,0 +1,5 @@
+export default {
+    appId: "19004c7c7b6b1e9", //Enter your App ID
+    apiKey: "c0c0303f5891a956a30e92779dfb1f5a5295933f", //Enter your API KEY
+    GUID: "testgroup", // Enter your group UID
+  };
